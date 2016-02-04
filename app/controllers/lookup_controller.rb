29d5c256fcc8d034d38bf3f49d990c844ac9f906
Jill Tankersley <jillian.emilie@gmail.com>
2016-02-03 19:32:04 -0500

    first commit

---
+++ app/controllers/lookup_controller.rb
@@ -0,0 +1,13 @@
+class LookupController < ApplicationController
+  def search
+  end
+
+  def results
+  	if params[:name] != nil
+	  	name_arr = params[:name].titleize.split
+	  	@results = Contact.where(first_name: name_arr[0], last_name: name_arr[1], user_id: current_user.id)
+	  elsif params[:city] != nil
+	  	@results = Contact.where(city: params[:city].titleize, user_id: current_user.id)
+	  end
+  end
+end
---
+++ db/migrate/20160203132930_add_attachment_avatar_to_contacts.rb
@@ -0,0 +1,15 @@
+class AddAttachmentAvatarToContacts < ActiveRecord::Migration
+  def self.up
+    change_table :contacts do |t|
+
+      t.attachment :avatar
+
+    end
+  end
+
+  def self.down
+
+    remove_attachment :contacts, :avatar
+
+  end
+end
